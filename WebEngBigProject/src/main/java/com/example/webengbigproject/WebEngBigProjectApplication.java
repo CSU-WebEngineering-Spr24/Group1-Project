23ff1591e3a7e0f33e2ef6772cf36a3776b5bbbf
Himanshu-Bohra <himanshubohra@hotmail.com>
2024-03-31 16:35:55 -0400

    Spring backend test for trivia game backend
    This is a test folder for the springboot backend, which also doubles as a backend program skeleton

---
+++ WebEngBigProject/src/main/java/com/example/webengbigproject/WebEngBigProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.webengbigproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEngBigProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WebEngBigProjectApplication.class, args);
+    }
+
+}
---
+++ WebEngBigProject/src/test/java/com/example/webengbigproject/WebEngBigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webengbigproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEngBigProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
