721cc7dac0f6e6ff52e42cb3b8e1025d5c27ad48
Himanshu-Bohra <himanshubohra@hotmail.com>
2024-05-05 04:34:37 -0400

    questions API call fix
    error handling wip

--- WebEngBigProject/src/main/java/com/example/webengbigproject/GlobalExceptionHandler.java
+++ WebEngBigProject/src/main/java/com/example/webengbigproject/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.example.webengbigproject;
 
 /**
- * @author : Himanshu Bohra
+ * @author Himanshu Bohra
  * This is an exception handler for all un-mapped api calls. Displays a help page.
  */
 
@@ -11,12 +11,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.util.Arrays;
+import java.util.List;
+
 @ControllerAdvice
 public class GlobalExceptionHandler
 {
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<Object> handleEverythingElse(NoHandlerFoundException e) {
+    public String handleEverythingElse(NoHandlerFoundException e) {
         // Custom page
+
         String customResponse = "<div id = \"main\">" +
                 "<b><i>You are seeing this page because you are not using the API call properly." +
                 "<br><br>For questions please use '/questions?' followed by 'mode=' (arcade/challenge/quick)." +
@@ -32,6 +36,31 @@ public class GlobalExceptionHandler
                 "document.body.style.backgroundColor = \"black\";" +
                 "</script>";
         // Return the custom response with a 404 Not Found status
-        return new ResponseEntity<>(customResponse, HttpStatus.NOT_FOUND);
+        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, customResponse, "ERROR");
+        //return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+        //return new ResponseEntity<>(customResponse, HttpStatus.NOT_FOUND);
+        //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Submission Failed: Internal Server Error");
+        return customResponse;
+    }
+}
+
+
+
+class ApiError {
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    // Additional constructor for single error message
+    public ApiError(HttpStatus status, String message, String error) {
+        this.status = status;
+        this.message = message;
+        this.errors = Arrays.asList(error);
     }
 }
