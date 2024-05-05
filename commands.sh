e274b1b3aadc2331c5cd0f5c04843441f9f0f5bc
Akshith <akshithnukala9@gmail.com>
2024-05-05 12:12:26 +0000

    commands

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
 mvn install 
 
 mvn spring-boot:run
+
+cd /workspace/Group1-Project/WebEngBigProject/src/main/react && npm run build && cd /workspace/Group1-Project/WebEngBigProject/ && mvn spring-boot:run
